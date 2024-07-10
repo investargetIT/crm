e1f2df684060e219b527c75bedd96487f3cbe0e1
Oliver <olimovier@gmail.com>
2024-07-10 15:57:48 +0800

    JPA接入测试

---
+++ src/main/java/com/cyanrock/oms/dao/User01Repository.java
@@ -0,0 +1,15 @@
+package com.cyanrock.oms.dao;
+
+import com.cyanrock.oms.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Create By IntelliJ IDEA
+ *
+ * @author LiLong
+ * @date 2020/12/24
+ */
+public interface User01Repository extends JpaRepository<User,Integer> {
+}
